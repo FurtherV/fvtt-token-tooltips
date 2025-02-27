5bf2d2f0bd83de2fae44305ddd6e96dcb5f1c90e
FurtherV <alex.howel@web.de>
2025-02-27 16:19:15 +0100

    fix typings

---
+++ foundry/globals.js
@@ -0,0 +1,12 @@
+/** @type {Notifications} */
+let notifications;
+
+/** @type {import("./resources/app/client-esm/applications/_types.mjs")} */
+let applications;
+
+/** @type {import("FClient")} */
+let foundry;
+
+var ui = {
+  notifications,
+};
